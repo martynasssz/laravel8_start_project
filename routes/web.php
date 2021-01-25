03f49cffe4f87f905ca703732a86f101b3b49b0b
unknown <j0h@example.com>
2021-01-25 21:16:33 +0200

    Middleware

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    echo "This is home page";
+});
+
 Route::get('/about', function () {
     return view('about');
-});
+})->middleware('check');
 
 //Route::get('/contact', 'ContactController@index');  //after @ we write method we want to create //laravel 6 ard 7 writing format
 

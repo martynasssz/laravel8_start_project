1b7b54ab89e92858c774f5a713030dcbd249c830
unknown <j0h@example.com>
2021-02-02 21:57:01 +0200

    Insert data with Query Buider

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Carbon\Carbon;
-use App\Models\Category;
+//use Carbon\Carbon;
+//use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
@@ -26,21 +26,28 @@ class CategoryController extends Controller
 
     //elequent method
 
-    Category::insert([
-        'category_name' => $request->category_name, //first database fiels  //$request function // name passed from DB name="category_name"
-        'user_id' => Auth::user()->id, //access only login user id  => means add //id will be taken this user who logged in
-        'created_at' => Carbon::now() //will be inserted carbon format
+    //Category::insert([
+      //  'category_name' => $request->category_name, //first database fiels  //$request function // name passed from DB name="category_name"
+      //  'user_id' => Auth::user()->id, //access only login user id  => means add //id will be taken this user who logged in
+      //  'created_at' => Carbon::now() //will be inserted carbon format
 
-    ]);
+   // ]);
 
-    return Redirect()->back()->with('success','Category inserted succesfully' );   //when will be inserted it wil redirect to backpage // with ('success', ) means what kind of message we want to see
-
-    // query bilder method
+    
+    // another method how to insert data
 
     // $category = new Category; //new object for category model
     // $category->category_name = $request->category_name ; //with new object  $category we access database field name and it should match with requested category_name from blade input name
     // $category->user_id = Auth::user()->id; //math with Authenticated user id
     // $category->save();
+
+    $data = array();
+    $data['category_name'] = $request->category_name;    //$data['category_name'] - database field  //  = $request->category_name  requested field 
+    $data['user_id'] = Auth::user()->id; 
+
+    DB::table('categories')->insert($data);  //insert all array data
+        
+    return Redirect()->back()->with('success','Category inserted succesfully' );   //when will be inserted it wil redirect to backpage // with ('success', ) means what kind of message we want to see
 
 
     }
